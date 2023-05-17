cfe69200d622bb1f5db2d01e5d9203f3597e1bad
Carlos <Carlos@Cano>
2023-05-18 00:59:39 +0200

    COMMIT INICIAL

---
+++ src/pruebas_github/Pruebas_GitHub.java
@@ -0,0 +1,16 @@
+package pruebas_github;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Pruebas_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primer commit");
+    }
+    
+}
